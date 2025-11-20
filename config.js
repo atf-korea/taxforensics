b2771a4e2fb26168a37ccecf215a242e2154174a
atf-korea <atf.kr@atfconsulting.org>
2025-11-20 10:00:56 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const GOOGLE_API_KEY = 'AIzaSyC_R9RRndaZFTZsVEOcuaHDEmtT990TRQg';
+const GOOGLE_SHEET_ID = '1K0sM-LtrctlacluIHUYSkCa4ZJWcSZ_NOmyrUvne6VE';
+const SHEET_RANGE = 'Tax_Hub!A1:D'; 
