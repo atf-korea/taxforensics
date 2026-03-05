de86f61c360b42d3c4cf38b85bcc342cfec00e01
atf-korea <atf.kr@atfconsulting.org>
2026-03-05 13:38:23 +0900

    Add Google API configuration details
    Added Google API key, sheet ID, and range to config.

--- forcass/config.js
+++ forcass/config.js
@@ -1,2 +1,5 @@
 // config.js
+const GOOGLE_API_KEY = 'AIzaSyBS1kIHPn1j2iCL4npiitSo_6Sm0M42450';
+const GOOGLE_SHEET_ID = '1OGxntagPpoC7nzQMpRVabBFcLo9wp-RVa3NCbXCgfes';
+const SHEET_RANGE = 'NTSP!A1:N'; 
 
